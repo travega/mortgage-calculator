67440e13749dbd57dbaad9aa802a33287f2daf96
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2018-10-31 13:59:47 +0000

    first commit

--- release/migration.py
+++ release/migration.py
@@ -9,28 +9,26 @@ dbname = db_parsed.path[1:]
 host = db_parsed.hostname
 port = db_parsed.port
 
-try:
-  connection = psycopg2.connect(dbname=dbname, host=host, port=port, user=user, password=pwd)
-  cur = connection.cursor()
+connection = psycopg2.connect(dbname=dbname, host=host, port=port, user=user, password=pwd)
+cur = connection.cursor()
 
-  query = """create schema if not exists mortgage_calculator;
+query = """create schema if not exists mortgage_calculator;
 
-  create sequence if not exists serial;
+create sequence if not exists serial;
 
-  create table if not exists mortgage_calculator.load_enquiries (
-    id integer default nextval('serial') not null
-      constraint load_enquiries_pkey
-      primary key,
-    username          varchar(100),
-    principal         decimal,
-    interest          decimal,
-    years             numeric,
-    enquiry_source    numeric,
-    timestamp         varchar(100),
-    created_at        timestamp default now()
-  );"""
+create table if not exists mortgage_calculator.load_enquiries (
+  id integer default nextval('serial') not null
+    constraint load_enquiries_pkey
+    primary key,
+  username          varchar(100),
+  principal         decimal,
+  interest          decimal,
+  years             numeric,
+  enquiry_source    numeric,
+  timestamp         varchar(100),
+  created_at        timestamp default now()
+);"""
 
-  cur.execute(query)
-  connection.close()
-except Exception as e:
-  print("Error occurred: {}".format(e))
+cur.execute(query)
+cur.execute("commit")
+connection.close()
