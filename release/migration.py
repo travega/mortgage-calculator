2ae189e9df81e57edcd214f30138f3548696755c
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2018-10-31 18:45:41 +0000

    first commit

--- release/migration.py
+++ release/migration.py
@@ -20,13 +20,13 @@ create table if not exists mortgage_calculator.load_enquiries (
   id integer default nextval('serial') not null
     constraint load_enquiries_pkey
     primary key,
-  username          varchar(100),
-  principal         decimal,
-  interest          decimal,
-  years             numeric,
-  enquiry_source    numeric,
-  timestamp         varchar(100),
-  created_at        timestamp default now()
+    username          varchar(100),
+    principal         decimal,
+    interest          decimal,
+    years             numeric,
+    enquiry_source    varchar(100),
+    timestamp         varchar(100),
+    created_at        timestamp default now()
 );"""
 
 cur.execute(query)
