ef3694e3ad3334ea951a1e60acdce9e9189f7191
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2018-10-31 13:07:56 +0000

    first commit

---
+++ release/migration.py
@@ -0,0 +1,32 @@
+from urllib.parse import urlparse
+import psychopg2
+
+db_parsed = urlparse(os.environ['DATABASE_URL'])
+user = db_parsed.username
+pwd = db_parsed.password
+dbname = db_parsed.path[1:]
+host = db_parsed.hostname
+port = db_parsed.port
+
+connection = psycopg2.connect(dbname=dbname, host=host, port=port, user=user, password=pwd)
+cur = connection.cursor()
+
+query = """create schema if not exists mortgage_calculator;
+
+create sequence if not exists serial;
+
+create table if not exists mortgage_calculator.load_enquiries (
+  id integer default nextval('serial') not null
+    constraint load_enquiries_pkey
+    primary key,
+  username          varchar(100),
+  principal         decimal,
+  interest          decimal,
+  years             numeric,
+  enquiry_source    numeric,
+  timestamp         varchar(100),
+  created_at        timestamp default now()
+);"""
+
+cur.execute(query)
+connection.close()
