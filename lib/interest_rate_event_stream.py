6e8c99ef80bddab1d6931824724b39de5dabbd5f
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2018-12-11 21:39:01 +0000

    rejig consumer

--- lib/interest_rate_event_stream.py
+++ lib/interest_rate_event_stream.py
@@ -10,10 +10,10 @@ topic = "{}{}".format(os.environ["KAFKA_PREFIX"], os.environ["TOPIC"])
 consumer = kafka_helper.get_kafka_consumer(topic=topic)
 print ("Connected: {}".format(topic))
 
-async def echo(websocket, path):
+async def echo():
     # async for message in websocket:
     for message in consumer:
         await print (message)
         # await websocket.send(json.dumps(message.value))
 
-asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_forever(echo())
