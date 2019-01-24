98214e0849f5ace32edafb7932aa3f044776fa56
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2019-01-24 00:13:58 +0000

    number format

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def consumer():
 
     json_data = json.loads(data)
 
-    principal = json_data["principal"]
+    principal = json_data["principal"].replace(",", "")
     interest = json_data["interest"]
     years = json_data["years"]
 
