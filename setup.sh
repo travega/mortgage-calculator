e176d141f9990607663742b498ee1710440c3dda
Andy Scott <travega@gmail.com>
2019-07-05 12:14:51 +0100

    config update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-heroku addons:create heroku-postgresql:hobby-basic --app $1
-heroku pg:wait
-
 heroku addons:create mongolab:sandbox --app $1
 heroku pg:wait
 
@@ -18,7 +15,7 @@ heroku pg:wait
 heroku config:add QUEUE_NAME="loan_enquiries" --app $1
 heroku pg:wait
 
-heroku config:set TOPIC="salesforce.interest_rate__e"  --app $1
+heroku config:set TOPIC="salesforce.interest_rate__e" --app $1
 heroku pg:wait
 
-heroku config -s --app $1 > .env
+heroku config -s --app $1 >.env
